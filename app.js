5fc745875b0b7b54f1f2493fbe346f12007ce8e6
Isaac Rodrigues <isaac.rodmed@gmail.com>
2026-06-09 15:49:27 -0300

    Adicionado login.js e corrigindo bug do dashboard e dalista

--- app.js
+++ app.js
@@ -1,90 +1,3 @@
-import { auth } from "./firebase.js";
-
-import {
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signInWithPopup,
-    GoogleAuthProvider
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-
-console.log("app.js carregado");
-
-//login
-
-const emailInput = document.getElementById("email");
-const senhaInput = document.getElementById("senha");
-const btnLogin = document.getElementById("btnLogin");
-const btnCadastro = document.getElementById("btnCadastro");
-const btnGoogle = document.getElementById("btnGoogle");
-
-if (btnLogin) {
-    btnLogin.addEventListener("click", async () => {
-        alert("Entrar clicado");
-
-        const email = emailInput.value.trim();
-        const senha = senhaInput.value.trim();
-
-        try {
-            const userCredential = await signInWithEmailAndPassword(auth, email, senha);
-
-            console.log(userCredential.user);
-
-            alert("Login feito com sucesso!");
-
-            window.location.href = "./dashboard.html";
-
-        } catch (error) {
-            console.log(error);
-            alert("Erro ao entrar: " + error.code);
-        }
-    });
-}
-
-if (btnCadastro) {
-    btnCadastro.addEventListener("click", async () => {
-        alert("Criar conta clicado");
-
-        const email = emailInput.value.trim();
-        const senha = senhaInput.value.trim();
-
-        try {
-            const userCredential = await createUserWithEmailAndPassword(auth, email, senha);
-
-            console.log(userCredential.user);
-
-            alert("Conta criada com sucesso!");
-
-            window.location.href = "./dashboard.html";
-
-        } catch (error) {
-            console.log(error);
-            alert("Erro ao criar conta: " + error.code);
-        }
-    });
-}
-
-if (btnGoogle) {
-    btnGoogle.addEventListener("click", async () => {
-        alert("Google clicado");
-
-        try {
-            const provider = new GoogleAuthProvider();
-
-            const userCredential = await signInWithPopup(auth, provider);
-
-            console.log(userCredential.user);
-
-            alert("Login Google feito com sucesso!");
-
-            window.location.href = "./dashboard.html";
-
-        } catch (error) {
-            console.log(error);
-            alert("Erro Google: " + error.code);
-        }
-    });
-}
-
 
 // DASHBOARD
 
@@ -128,9 +41,7 @@ function carregarListas() {
 carregarListas();
 
 
-
 // CRIAR LISTA
-
 
 const plusBtn =
     document.querySelector(".plus-btn");
@@ -176,7 +87,6 @@ if (plusBtn) {
 
 }
 
-
 // CARREGAR LISTA
 
 const nomeListaInput =
@@ -218,13 +128,14 @@ if (
 
             lista.nome =
                 nomeListaInput.value;
-
-            salvar();
+        
+            salvar(lista);
 
         }
     );
 
 }
+
 
 // ADICIONAR ITEM
 
@@ -379,9 +290,7 @@ function renderizarItens() {
 }
 
 
-// 
 // VOLTAR
-// 
 
 const btnVoltar =
     document.getElementById(
@@ -455,25 +364,24 @@ if (btnApagarLista) {
 }
 
 
+
 // SALVAR
 
-function salvar() {
+function salvar(listaAtualizada) {
 
     let listas =
         JSON.parse(
-            localStorage.getItem(
-                "listas"
-            )
+            localStorage.getItem("listas")
         ) || [];
 
     const index =
         listas.findIndex(
-            l =>
-            l.id ===
-            listaAtualId
+            l => l.id === listaAtualId
         );
 
-    listas[index] = lista;
+    if (index === -1) return;
+
+    listas[index] = listaAtualizada;
 
     localStorage.setItem(
         "listas",
